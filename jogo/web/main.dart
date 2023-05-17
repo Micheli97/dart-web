d6bd1eb795d29c923aa7bc12f9e9c075168e0920
Paula Micheli <paula.michele.581@gmail.com>
2023-05-17 15:10:45 -0300

    feat(): Finalizando jogo com exibição do resultado e reinicialização do jogo

--- jogo/web/main.dart
+++ jogo/web/main.dart
@@ -1,19 +1,72 @@
 import 'dart:html';
 import 'package:jogo/src/partida.dart';
+import 'package:jogo/src/resultado.dart';
 
+typedef EventListener(Event event);
 late DivElement divOpcoes;
 late DivElement divResultado;
+bool jogando = true;
+late Resultado resultado;
 void main() {
   inicializarReferencias();
-  configurarPartida();
+  final partida = configurarPartida();
   print(opcoes.length);
 
   for (final opcao in opcoes) {
     divOpcoes.append(ImageButtonInputElement()
+      ..onClick.listen((MouseEvent event) {
+        if (jogando) {
+          resultado = partida.iniciar(humano: opcao);
+          jogando = false;
+          mostrarResultado(resultado);
+        }
+      })
       ..className = "opcao"
       ..src = 'images/$opcao.png'
       ..height = 120);
   }
+}
+
+void mostrarResultado(Resultado resultado) {
+  String mensagem;
+  String classeCss;
+
+  switch (resultado.resultadoType) {
+    case ResultadoType.empate:
+      classeCss = "empatou";
+      mensagem = "Empatou..";
+      break;
+    case ResultadoType.vitoria:
+      classeCss = "venceu";
+      mensagem = "Você ganhou :D";
+      break;
+    case ResultadoType.derrota:
+      classeCss = "perdeu";
+      mensagem = "Você perdeu :/";
+      break;
+  }
+
+  divResultado.append(SpanElement()
+    ..className = classeCss
+    ..text = mensagem);
+
+  adicionarEspaco();
+  divResultado.append(SpanElement()..text = resultado.resumo);
+  adicionarEspaco();
+
+  divResultado.append(ButtonElement()
+    ..text = "Jogar novamente"
+    ..onClick.listen(jogarNovamente));
+}
+
+void adicionarEspaco() {
+  divResultado.append(BRElement());
+  divResultado.append(BRElement());
+}
+
+void jogarNovamente(MouseEvent event) {
+  jogando = true;
+  divResultado.children.clear();
 }
 
 void inicializarReferencias() {
